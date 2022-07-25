57ee82cd39d417614575db3d5706cc1e3c512ca1
sukrusimsek <107575752+sukrusimsek@users.noreply.github.com>
2022-07-25 14:43:30 +0300

    Update degiskenler.py

--- degiskenler.py
+++ degiskenler.py
@@ -2,7 +2,7 @@ sayi = 10 #integer yani rakamsal
 tcno = "39021454868" #string yani metinsel
 isim = "sukru"
 soyIsim = "simsek"
-telefonNumarasi = "5336976623"
+telefonNumarasi = "5336945423"
 floatSayi = 10
 print(float(tcno) + 10)
 print(type(floatSayi))
